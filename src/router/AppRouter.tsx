10bcfffc49dd15a1bbbbcee338023ad2f637f6c4
ronaldoisc <tellezleal9928@gmail.com>
2022-02-23 00:06:59 -0600

    update routes

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,18 +3,16 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate,
+  // Navigate,
 } from "react-router-dom";
 
 
-import { Home } from "../containers/Home/Home";
-import { Albums } from '../containers/Albums/Albums';
-import { Tracks } from '../containers/Tracks/Tracks';
-import { TrackDetails } from '../containers/Tracks/TrackDetails';
+
 import { useEffect } from 'react';
 import { useDispatch, useSelector, RootStateOrAny } from 'react-redux';
 import { startCheckingToken } from "../redux/modules/newReleases";
 import { Loader } from "../components/Loader";
+import { DashboardRoutes } from "./DashboardRoutes";
 
 
 export const AppRouter = () => {
@@ -35,11 +33,16 @@ export const AppRouter = () => {
     <Router>
       <div>
         <Routes>
-          <Route  exact path="/" element={<Home />} />
-          <Route  exact path="/albums/:artistId" element={<Albums />} />
-          <Route  exact path="/album/:albumId/tracks" element={<Tracks />} />
-          <Route  exact path="/track/:trackId" element={<TrackDetails />} />
-          <Route  exact path="/*" element={<Navigate to={"/"} replace />} />
+          <Route path="/*" element={
+            <DashboardRoutes/>
+          }/>
+
+         
+          {/* <Route  path="/" element={<Home />} />
+          <Route path="/albums/:artistId" element={<Albums />} />
+          <Route path="/album/:albumId/tracks" element={<Tracks />} />
+          <Route path="/track/:trackId" element={<TrackDetails />} />
+          <Route path="/*" element={<Navigate to={"/"} replace />} /> */}
         </Routes>
       </div>
     </Router>
--- src/router/DashboardRoutes.tsx
+++ src/router/DashboardRoutes.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { Albums } from '../containers/Albums/Albums'
 import { Tracks } from '../containers/Tracks/Tracks'
+import { Home } from '../containers/Home/Home';
+import { TrackDetails } from '../containers/Tracks/TrackDetails';
 
 export const DashboardRoutes = () => {
   return (
     <>
       <Routes>
-        <Route path=":artistId" element={<Albums />} />
-        <Route path=":albumId/tracks" element={<Tracks />} />
+          <Route  path="/" element={<Home />} />
+          <Route path="/albums/:artistId" element={<Albums />} />
+          <Route path="/album/:albumId/tracks" element={<Tracks />} />
+          <Route path="/track/:trackId" element={<TrackDetails />} />
+          <Route path="/*" element={<Navigate to={"/"} replace />} />
       </Routes>
 
     </>
